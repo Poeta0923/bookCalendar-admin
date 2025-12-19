d4598f5286da3f8964155f10bb315d5f42097621
Kwangsoo Kim <132874295+Poeta0923@users.noreply.github.com>
2025-12-20 02:08:54 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 const logger = require('./lib/logger');
 
-//세션 저장소 관련 설정. mariadb npm 패키지는 정상 작동하지 않는 경우가 많아 express-mysql-session 사용
-//추후 서버에서 테스트 시 정보 변경 예정
+//세션 저장소 관련 설정
 var MySqlStore = require('express-mysql-session')(session);
 var options = {
     host : 'localhost',
@@ -16,7 +15,7 @@ var options = {
 
 var sessionStore = new MySqlStore(options)
 
-//세션 관련 설정. 세션 비밀 키는 깃허브 repository가 public이라 미기입
+//세션 관련 설정. 세션 비밀 키는 깃허브 repository가 public이라 미끼입
 const app = express();
 app.use(session({
     secret : 'not decided yet',
@@ -25,7 +24,7 @@ app.use(session({
     store : sessionStore
 }));
 
-//ejs 파일들 관련 설정 (아래의 주석처리는 개발할 때 빼오기)
+//ejs 파일들 관련 설정
 app.set('views',__dirname+'/views');
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: false}));
@@ -52,7 +51,7 @@ app.use('/admin/statistics', statisticsRouter);
 //favicon 파일이 없기 때문에 브라우저가 요청 시 에러 처리.
 app.get('/favicon.ico', (req, res)=>res.writeHead(404));
 
-//포트 번호는 수정 예정. callback 함수는 개발 이후에도 로깅과 디버깅을 위해 남겨둘 예정
+//포트 번호는 nginx 정적 파일 전송을 위해 내부 포트 사용. callback 함수는 개발 이후에도 로깅과 디버깅을 위해 남겨둘 예정
 app.listen(3005, '0.0.0.0', ()=>{
     logger.info(`Server listening on port 60001`);
     console.log('Example app listening on port 60001');
